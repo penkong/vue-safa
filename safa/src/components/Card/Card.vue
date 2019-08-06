efe7c03285a188bfd29d3dc0eab09f814c296b51
TMP <tmp@safarayaneh.local>
2019-08-06 12:23:02 +0430

    make cards responsive done

--- safa/src/components/Card/Card.vue
+++ safa/src/components/Card/Card.vue
@@ -1,15 +1,13 @@
 <template>
-  <div
-    class="card w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500 rounded overflow-hidden shadow-lg"
-  >
-    <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+  <div class="card">
+    <!-- <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" /> -->
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
       <p
         class="text-gray-700 text-base"
       >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
     </div>
-    <div class="px-6 py-4">
+    <!-- <div class="px-6 py-4">
       <span
         class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
       >#photography</span>
@@ -19,7 +17,7 @@
       <span
         class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
       >#winter</span>
-    </div>
+    </div>-->
   </div>
 </template>
 
@@ -30,5 +28,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../sass/abstract/mixins";
 @import "Card";
 </style>
